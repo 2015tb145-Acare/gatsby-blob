1124c1e047a2f87ecaf1fbf2d23053b689c1e4be
2015tb145-Acare <alban.care@gmail.com>
2022-09-12 02:33:17 +0200

    Fix: Amélioration des performances en utilisant des polices locales

---
+++ src/styles/fonts.js
@@ -0,0 +1,69 @@
+import { css } from "styled-components";
+
+const Fonts = css`
+  @font-face {
+    font-family: "Poppins-Thin";
+    font-style: normal;
+    font-weight: 100;
+    font-display: swap;
+    src: url(/fonts/Poppins-Thin.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-ExtraLight";
+    font-style: normal;
+    font-weight: 200;
+    font-display: swap;
+    src: url(/fonts/Poppins-ExtraLight.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-Light";
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: url(/fonts/Poppins-Light.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-Regular";
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(/fonts/Poppins-Regular.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-Medium";
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+    src: url(/fonts/Poppins-Medium.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-SemiBold";
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url(/fonts/Poppins-SemiBold.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-Bold";
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: url(/fonts/Poppins-Bold.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-ExtraBold.ttf";
+    font-style: normal;
+    font-weight: 800;
+    font-display: swap;
+    src: url(/fonts/Poppins-ExtraBold.ttf) format("ttf");
+  }
+  @font-face {
+    font-family: "Poppins-Black.ttf";
+    font-style: normal;
+    font-weight: 900;
+    font-display: swap;
+    src: url(/fonts/Poppins-Black.ttf) format("ttf");
+  }
+`;
+
+export default Fonts;
